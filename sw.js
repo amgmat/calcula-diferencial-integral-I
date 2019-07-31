6ae949a8e6c07ba67650bd0916c29a5c6e57fedc
Ale <amg_29-90@hotmail.com>
2019-07-31 11:06:35 -0500

    pronto

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('js/sw-utils.js');
 
 // console.log('Hola');
 
-const STATIC_CACHE = 'static-v4';
+const STATIC_CACHE = 'static-v5';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
