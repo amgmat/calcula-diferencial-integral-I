7453a88a72735d8398e69a5975ba572af3183e83
Ale <amg_29-90@hotmail.com>
2019-08-07 12:49:51 -0500

    v3

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('js/sw-utils.js');
 
 // console.log('Hola');
 
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v3';
 const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
